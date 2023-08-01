1e7673fe929285c6c988c2a7d11182107eb5a393
DeltaFROST141 <51741026+DeltaFROST141@users.noreply.github.com>
2023-08-01 23:14:44 +0800

    P3 to P4 and the last part of P4 is remaining(sum, softmax...)

--- FMP/Relearning/ListLearning.py
+++ FMP/Relearning/ListLearning.py
@@ -1,5 +1,17 @@
-shopping_list = ["Giant/Liv"]
+shopping_list = ["Giant/Liv", "Keyboard", "Mouse", "iPad Pro"]
 
 shopping_list.append("Airpods Max")
+shopping_list.remove("Keyboard")
+
+#
+
+for items in shopping_list:
+    print(f"I would like to buy {items}")
 
 print(shopping_list)
+print(len(shopping_list))
+
+GSW_num_list = [9, 11, 23, 30, 35]
+print(max(GSW_num_list))
+print(min(GSW_num_list))
+print(sorted(GSW_num_list))
