d6af234d816bf7540907dd54dc46be8448c41605
Chuqi Zhou <chuqizhou30@qq.com>
2023-10-24 21:42:50 +0800

    Including the following functions: 1. loop over each sequence(row) in the two dimensional array 2. divide a sequence into several windows 3. in every sub windows, calculate the permutation entropy and output the entropy list

--- FMP/My_Code/GetEntropyChannel.py
+++ FMP/My_Code/GetEntropyChannel.py
@@ -2,36 +2,47 @@ import numpy as np
 import itertools
 from PECalc import pea
 
+np.seterr(divide='ignore',invalid='ignore')
+
 def mapminmax(arr, mi, mx):
-    return (arr - np.min(arr)) * (mx - mi) / (np.max(arr) - np.min(arr)) + mi 
+    return (arr - np.min(arr)) * (mx - mi) / (np.max(arr) - np.min(arr)) + mi
 
 
 def getentropy(x, w):
-    
     t, l = x.shape
     # ! divide the long trail into sub trails, trails number are g
-    g = l // w  
+    g = l // w
+    print('窗口数量为: {}'.format(g))
 
     # ! can find the maximum and minimum directly
     mi = np.min(x)
     mx = np.max(x)
 
-    # ! store the entropy values
+    # ! store the entropy values for each window
     # for example: (1, 3, 1)
     # means that find the second trails and the fourth windows
-    # and the number of [(0, 2, 1)]
+    # and the frequency of occurrence(number) of [(0, 2, 1)]
     entropy = np.zeros((t, g, 6))
-    
+
     # ! loop over each window and calculate the entropy
+    # 每个窗口中再进行滑动窗口进行熵计算！
+    # 
     for i in range(t):
         x1 = x[i, :]
+        print(x1)
         for k in range(g):
-            entropy[i, k, :] = pea(x1[(k * w):((k + 1) * w)] ,3 ,1)
-            
+            print("当前序列正在遍历的窗口是:")
+            print(x1[(k * w):((k + 1) * w)])
+            entropy[i, k, :] = pea(x1[(k * w):((k + 1) * w)], 3, 1)
+
+        print("该条序列遍历结束")    # print(entropy[i, k, :])
+    print("全部数据遍历结束")
+
     entropy = np.reshape(mapminmax(np.reshape(entropy, -1), mi, mx), (t, g, 6))
-    
+
+    # ! select two trials and compare
     comb_indices = list(itertools.combinations(range(t), 2))
-    y = np.zeros((len(comb_indices), 2, 2 * g))
+    y = np.zeros((len(comb_indices), 4, 2 * g))
 
     # ! for each pair of (i, j), calculate the min and max
     # ! values.
@@ -48,13 +59,22 @@ def getentropy(x, w):
             e2_min = min_entro2[k]
             e2_max = max_entro2[k]
 
-            E1_min, E2_min = np.meshgrid(e1_min, e1_max)
-            E1_max, E2_max = np.meshgrid(e2_min, e2_max)
+            # E1_min, E2_min = np.meshgrid(e1_min, e1_max)
+            # E1_max, E2_max = np.meshgrid(e2_min, e2_max)
 
             # ! use the matlab code idea
             # ! represent the overlapping structure
-            y[idx, :, 2 * k] = np.column_stack((E1_min.ravel(), E1_max.ravel()))
-            y[idx, :, 2 * k + 1] = np.column_stack((E2_min.ravel(), E2_max.ravel()))
-    
+            E1, E2 = np.meshgrid([e1_min, e1_max], [e2_min, e2_max])
+            E_combined = np.column_stack((E1.ravel(), E2.ravel()))
+            y[idx, :, 2 * k:2 * k + 2] = E_combined
+
     # print(y)
     return y
+
+
+
+array = np.random.randint(0, 10, size=(3, 5000))
+y = getentropy(array, 20)
+print("最终数组：")
+print(y)
+print(y.shape)
--- FMP/My_Code/PECalc.py
+++ FMP/My_Code/PECalc.py
@@ -1,42 +1,46 @@
 import numpy as np
 from itertools import permutations
 
-# ! Only calculate the single trial entropy and store them(each index entropy) in a list 
+# ! Only calculate the single trial entropy and store them(each index entropy) in a list
 def pea(y, m, t):
     ly = len(y)
-    
+
     permlist = list(permutations(range(m)))
     c = np.zeros(len(permlist))
     print(permlist)
 
     # ! Be advise the range
     for j in range(ly - t * (m - 1) - t + 1):
+        print("当前窗口的子窗口为：")
+        print(y[j : j + t * (m - 1) + t : t])
         sorted_idx = list(np.argsort(y[j : j + t * (m - 1) + t : t]))
         for jj, perm in enumerate(permlist):
             if sorted_idx == list(perm):
                 c[jj] += 1
-    
-    print("The numbers:")
+
+    print("记录对应排序出现次数的数组为:")
     print(c)
     if np.sum(c) == 0:
         raise ValueError("The sum of permutation counts is zero!")
 
-    # ! The number list   
+    # ! The number list
     p = c / np.sum(c)
-    print("The prop:")
+    print("记录对应排序出现概率的数组为:")
     print(p)
-    
+
     # ! Also the list, but the probability list
     pe = np.zeros_like(c)
     for i, count in enumerate(c):
         if count != 0:
             # ! Entropy calculation
             pe[i] = -p[i] * np.log2(p[i])
-    print("The entropy list:") 
+    print("记录排序熵的数组为:")
+    print(pe)
     return pe
 
 # ? Why to use sort function instead of the directly calculation?
-y = [4, 7, 9, 10, 6, 11, 3]
-m = 3
-t = 1   
-print(pea(y, m, t))
+# y = np.array([[4, 7, 9, 10, 6, 11, 3]
+#               ,[1, 2, 3, 4, 1, 2, 3]])
+# m = 3
+# t = 1
+# print(pea(y, m, t)) # 直接输出返回值 pe 数组
